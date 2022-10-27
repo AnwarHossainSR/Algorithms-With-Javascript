02b62c000b53111802c46a11a3adbe4aeae7e0f6
Anwar Hossain <mahedianwar@gmail.com>
2022-10-27 11:41:23 +0600

    prime number problem solved

---
+++ primeNumberProblem.js
@@ -0,0 +1,13 @@
+const isPrimeNumber = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPrimeNumber(6));
